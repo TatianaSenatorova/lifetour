4e18bff0754202a856108273b8c0aba829b5ab91
Tatiana Senatorova <1975tanya@bk.ru>
2025-04-24 16:46:21 +0300

    feat: доделан функционал добавления и удаления дополнительных слайдов в блоке advantages

--- source/js/advantages-slider.js
+++ source/js/advantages-slider.js
@@ -9,42 +9,42 @@ import {
 } from './constants.js';
 
 let currentSlidesNumber = advantagesSlides.length;
+let advantagesSwiper;
 
-const changeSlidesNumber = () => {
+const addAdditionalSlides = () => {
   advantagesSlides.forEach((slide) => {
     const clonedSlide = slide.cloneNode(true);
     advantagesSlidesList.appendChild(clonedSlide);
     currentSlidesNumber++;
   });
-  console.log(currentSlidesNumber);
 }
 
 const removeAdditionalSlides = () => {
-const currentSlides = advantagesSlidesList.querySelectorAll('.advantages__slider-item');
-console.log(currentSlides);
-currentSlides.forEach((slide, index) => {
+  const currentSlides = advantagesSlidesList.querySelectorAll('.advantages__slider-item');
+  console.log(currentSlides);
+  currentSlides.forEach((slide, index) => {
     if (index > INIT_NUMBER_ADVANTAGES_SLIDES - 1) {
-      console.log(slide);
       slide.remove();
       currentSlidesNumber--;
     }
   });
-  console.log(advantagesSlidesList.querySelectorAll('.advantages__slider-item'));
-  console.log(currentSlidesNumber);
 }
 
 const checkDocumentWidth = () => {
   if (document.documentElement.clientWidth >= 1440 && currentSlidesNumber > INIT_NUMBER_ADVANTAGES_SLIDES) {
     return;
   } else if (document.documentElement.clientWidth >= 1440 && currentSlidesNumber <= INIT_NUMBER_ADVANTAGES_SLIDES) {
-    changeSlidesNumber();
-    return
+    addAdditionalSlides();
+    initSlider();
+    return;
   }
   removeAdditionalSlides();
+  advantagesSwiper.destroy();
 }
 
 const onDocumentDomContentLoaded = () => {
   checkDocumentWidth();
+  initSlider();
 }
 
 const onWindowResize = () => {
@@ -52,31 +52,34 @@ const onWindowResize = () => {
 }
 
 document.addEventListener("DOMContentLoaded", onDocumentDomContentLoaded);
+
 window.addEventListener("resize", onWindowResize);
 
-const advantagesSwiper = new Swiper('.advantages__slider', {
-  modules: [Navigation],
-  direction: 'horizontal',
-  loop: true,
-  enabled: false,
+const initSlider = () => {
+  advantagesSwiper = new Swiper('.advantages__slider', {
+    modules: [Navigation],
+    direction: 'horizontal',
+    loop: true,
+    enabled: false,
 
-  breakpoints: {
-    // 768: {
-    //   enabled: false,
-    // },
+    breakpoints: {
+      1440: {
+        enabled: true,
+        slidesPerView: 'auto',
+        slidesPerGroup: 2,
+        initialSlide: 2,
+        centeredSlides: true,
+        spaceBetween: 30,
+      }
+    },
 
-    1440: {
-      enabled: true,
-      slidesPerView: 'auto',
-      slidesPerGroup: 2,
-      initialSlide: 2,
-      centeredSlides: true,
-      spaceBetween: 30,
-    }
-  },
+    navigation: {
+      nextEl: '.swiper-button-advantages-next',
+      prevEl: '.swiper-button-advantages-prev'
+    },
+  });
+}
 
-  navigation: {
-    nextEl: '.swiper-button-advantages-next',
-    prevEl: '.swiper-button-advantages-prev'
-  },
-});
+
+
+
