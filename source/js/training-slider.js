d50b19a64042ba8e4d15c494021e38a4f87b8e22
Tatiana Senatorova <1975tanya@bk.ru>
2026-04-27 19:04:20 +0300

    fix: делает карточки инструторов резиновыми, удаляет лишние фото

--- source/js/training-slider.js
+++ source/js/training-slider.js
@@ -1,28 +1,34 @@
-import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
+import Swiper from "swiper";
+import { Navigation } from "swiper/modules";
 
-export const trainingSwiper = new Swiper('.swiper-training', {
+export const trainingSwiper = new Swiper(".swiper-training", {
   modules: [Navigation],
-  direction: 'horizontal',
+  direction: "horizontal",
   slidesPerView: 1,
   initialSlide: 2,
 
   breakpoints: {
+    500: {
+      initialSlide: 0,
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+
     768: {
       initialSlide: 0,
       slidesPerView: 3,
       spaceBetween: 20,
     },
 
-    1440: {
+    1120: {
       initialSlide: 0,
       slidesPerView: 4,
       spaceBetween: 20,
-    }
+    },
   },
 
   navigation: {
-    nextEl: '.swiper-button-training-next',
-    prevEl: '.swiper-button-training-prev'
+    nextEl: ".swiper-button-training-next",
+    prevEl: ".swiper-button-training-prev",
   },
 });
